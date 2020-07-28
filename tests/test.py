5c7ec4aa658071fde411d6c64663f4ab0e008bee
iisabeller <30798018+iisabeller@users.noreply.github.com>
2020-07-28 13:59:24 -0400

    add test.py

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import pytest
+from ElectricityIntensityIndicators.outline import LMDI
+from ElectricityIntensityIndicators.residential.ahs import * 
+
+# class TestClass:
+
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 5
