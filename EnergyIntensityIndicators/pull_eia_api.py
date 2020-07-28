ee6e7e4efc2e77e84e910f8a0d6635dccb603d7a
iisabeller <30798018+iisabeller@users.noreply.github.com>
2020-07-28 13:38:45 -0400

    add AHS

--- EnergyIntensityIndicators/pull_eia_api.py
+++ EnergyIntensityIndicators/pull_eia_api.py
@@ -1,14 +1,26 @@
 import os
 import json
 import requests
+import pandas as pd
+import numpy as np
 
+print('current directory', os.getcwd())
 api_key = os.environ.get("EIA_API_Key")
 
-
-api_category_ids = {'EIA Datsets': 371, 'seds_consumption_residential': 40209}
-category = 'EIA Datsets'
+api_category_ids = {'EIA Datsets': 371, 'seds_consumption_all': 40204, 'seds_consumption_residential': 40209}
+category = 'seds_consumption_all'
 call  = f'http://api.eia.gov/category/?api_key={api_key}&category_id={api_category_ids[category]}'
 
 r = requests.get(call)
 files = r.json()
 print(files)
+
+call2 = f'http://api.eia.gov/series/?api_key={api_key}&series_id=SEDS.PATCB.AL.A'
+r = requests.get(call2)
+j = r.json()
+# df = pd.DataFrame.from_dict(j)
+print(j)
+# print(df)
+
+consumption_all_btu = pd.read_csv('./EnergyIntensityIndicators/use_all_btu.csv')
+print(consumption_all_btu.head())
