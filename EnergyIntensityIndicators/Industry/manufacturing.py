da2d2083b703dc32569bad957a25745980dd58fe
iisabeller <30798018+iisabeller@users.noreply.github.com>
2020-08-18 11:59:59 -0400

    Industrial

---
+++ EnergyIntensityIndicators/Industry/manufacturing.py
@@ -0,0 +1,30 @@
+
+ASMdata_date.xlsx
+
+ind_hap3_date.xlsx
+
+2014_MECS = 'https://www.eia.gov/consumption/manufacturing/data/2014/'  # Table 4.2
+
+
+# Table 3.1 and 3.2 (MECS total fuel consumption)  Table 3.1 shows energy
+# consumption by fuel in physical units, including the total across all fuels expressed in trillion Btu and
+# electricity in kWh. From Table 3.1, total fuel consumption in Btu can be calculated as difference between
+# total energy and electricity consumption after conversion to Btu. Table 3.2 only differs from Table 3.1 by
+# showing all fuel types in Btu. 
+
+
+
+MER_Table24_Industrial_Energy_Consumption = 
+
+# For 2014, the values for total energy consumption and electricity consumption, both defined in terms of
+# trillion Btu, from Table 3.2 are transferred to spreadsheet ind_hap3. Worksheet MECS_Fuel in this
+# spreadsheet has been used to collect the fuel consumption estimates for all the MECS dating back to the
+# first MECS in 1985. The 2014 data are located in the cell range F218:F238.
+# The first six NAICS sectors are aggregated into three sectors (311-312, 313-314, and 315-316) as a part
+# of the set of manufacturing indicators. The energy consumption data under this revised sectoring
+# classification are shown in the columns to the right, columns O and P.
+
+
+
+# Energy prices
+MECS_Table72 = 
---
+++ EnergyIntensityIndicators/Industry/nonmanufacuturing.py
@@ -0,0 +1,56 @@
+
+class NonManufacturing:
+    """ Prior to 2012, total nonmanufacturing
+    energy consumption (electricity and fuels) was estimated as a residual between the supply-side
+    estimates of industrial consumption published by EIA and the end-user estimates for manufacturing
+    based upon the MECS (supplemented by census-based data, as described above). The residual-based
+    method produced very unsatisfactory results; year-to-year changes in energy consumption were
+    implausible in a large number of instances. A complicating factor for fuels is that industrial consumption
+    estimates published by EIA include energy products used as chemical feedstocks and other nonfuel
+    purposes. As a result, a preliminary effort was undertaken in mid-2012 to estimate energy consumption
+    from the user side for these sectors.   
+
+
+    """    
+    def __init__(self):
+        pass
+
+    def agriculture(self):
+        miranowski_data =  # Annual Estimates of energy by fuel for the farm sector for the period 1965-2002
+        nass_expenses_data =  # 
+        nass_average_prices_data =  # 
+        MER_fuel_price_data =  # 
+        pass
+
+    def mining(self):
+        """The energy consumption estimates for mining depend entirely on the various editions of the periodic
+        census (ending in years with ‘2” and “7” since 1967). Up through 1987, the information for mining was
+        collected under the title “Census of Mineral Industries.” From 1992 forward, the same information is
+        part of the mining segment of the Economic Census (which now is the broad term for all the census
+        surveys in the census years).
+        Table A.10 shows the website data sources for the mining sector. For the most recent census in 2007,
+        the data were selected from a flexible download procedure that allows the user to select key data
+        elements for each specific NAICS sector. The specific data items were 1) “quantity of electricity
+        purchased” and 2) fuels consumed by type: a) quantity, and b) delivered cost. For the previous years,
+        the data were derived from downloaded industry series reports (or selected pages). In these reports,
+        the cost and quantity of electricity is found in Table 3 (Detailed Statistics by Industry) and Table 7
+        (Selected Supplies, Minerals Received for Preparation, Purchased Machinery, and Fuels Consumed by
+        Type)
+
+            Since 1997, the mining industries have been classified under three major 3-digit NAICS sectors: 211, Oil
+        and Gas Extraction; 212, Mining (except oil and gas); 213 Support Activities for Mining. Unfortunately,
+        there are no aggregations of energy data from the more detailed industries to this level. Thus, an
+        estimation of electricity and fuel consumption must begin with the more detailed mining sectors,
+        essentially 6-digit NAICS since 1997 and 4-digit SIC in earlier years. At the NAICS level, there are 29
+        specific industries as shown in Table A.11 (the word “mining” has been omitted from most of the official
+        NAICS titles in the table). 
+        """        
+        pass
+        mining_2017 = 'https://www.census.gov/data/tables/2017/econ/economic-census/naics-sector-21.html'
+        mining_2012 = 'https://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml?src=bkmk'
+        mining_2007 = 'http://factfinder2.census.gov/faces/tableservices/jsf/pages/productview.xhtml?pid=ECN_2007_US_21SG12&prodType=table'
+        mining_2002 = 'https://www.census.gov/econ/census02/guide/INDRPT21.HTM'  # extract Table 3 and Table 7
+        mining_1997 = 'http://www.census.gov/prod/www/abs/ec1997mining-ind.html'  # extract Table 3 and Table 7
+        mining_1992 = 'http://www.census.gov/prod/1/manmin/92mmi/92minif.html'   # extract Table 3 and Table 7
+    def construction(self):
+        pass
